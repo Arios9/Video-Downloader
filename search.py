adf9b8df15a29ec20d880f9dc1705fdd5a4de3e7
Arios <ariosssmfm@gmail.com>
2021-10-16 02:41:32 +0300

    first

---
+++ search.py
@@ -0,0 +1,31 @@
+
+import re
+from bs4 import BeautifulSoup
+import urllib.parse
+from pytube import YouTube
+import os
+from pathlib import Path
+from selenium import webdriver
+
+youtubeSearchUrl = "https://www.youtube.com/results?search_query="
+webdriverPath = "C:\chromedriver_win32\chromedriver.exe"
+youtubeUrl = "https://www.youtube.com"
+searchText = str(input("Search "))
+searchTextEncoded = urllib.parse.quote(searchText)
+searchResultsUrl = youtubeSearchUrl + searchTextEncoded
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(webdriverPath, options=options)
+driver.get(searchResultsUrl)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+link = soup.find(href=re.compile(re.escape("/watch?v=")), id="thumbnail")
+youtubeVideoUrl = youtubeUrl + link['href']
+try:
+    yt = YouTube(youtubeVideoUrl)
+    stream = yt.streams.get_by_itag(18)
+    downloadPath = os.path.join(Path.home(), "Downloads")
+    fileName = yt.title+'.mp4'
+    stream.download(output_path=downloadPath, filename=fileName)
+    os.startfile(os.path.join(downloadPath, fileName))
+except:
+    print("Error")
